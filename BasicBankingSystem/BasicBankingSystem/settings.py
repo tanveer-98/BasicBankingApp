e69150a5354fefc2f9c43993ed2a47b3fc401a6f
tanveer-98 <43697498+tanveer-98@users.noreply.github.com>
2021-03-06 12:30:50 +0530

    commit 3 : added files for heroku hosting

--- BasicBankingSystem/BasicBankingSystem/settings.py
+++ BasicBankingSystem/BasicBankingSystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '91d-(jlwo1#(x*-1(fc(eahn26@_b91@1x)ws(u+l0i%l3x)g*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- BasicBankingSystem/BasicBankingSystem/urls.py
+++ BasicBankingSystem/BasicBankingSystem/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
+
+from django.views.static import serve
+from django.conf.urls.static import static
+
 import app1
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app1.urls'))
+    path('',include('app1.urls')),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+
 ]
