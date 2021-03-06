08ddcfedc467046dc2980e57d50b55b0ef2a4c35
tanveer-98 <43697498+tanveer-98@users.noreply.github.com>
2021-03-06 15:18:58 +0530

    commit 5:update for heroku

--- BasicBankingSystem/BasicBankingSystem/settings.py
+++ BasicBankingSystem/BasicBankingSystem/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
+import dj_database_url
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 STATIC_DIR
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
